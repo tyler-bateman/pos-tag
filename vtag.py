5c2b46723e4c181757966a948b2b367b6ab1678b
Tyler Bateman <43663440+tyler-bateman@users.noreply.github.com>
2020-05-24 12:25:40 -0700

    viterbi tagger now has the correct accuracy on the ic dataset

--- vtag.py
+++ vtag.py
@@ -21,18 +21,16 @@ def trainEmission(trainData):
         twCounts[tag][word] += 1
 
     for tag in twCounts:
-        #print(tag)
         totalLog = log(sum(twCounts[tag].values()))
         tw[tag] = {}
         for word in twCounts[tag]:
+            tw[tag][word] = log(twCounts[tag][word]) - totalLog if twCounts[tag][word] > 0 else -float('inf')
 
-            tw[tag][word] = log(twCounts[tag][word]) - totalLog if twCounts[tag][word] > 0 else None
 
 def trainTransition(trainData):
     ttCounts = dict.fromkeys(tagSet, {})
     for tag in ttCounts:
         ttCounts[tag] = dict.fromkeys(tagSet, 0)
-
 
     for i in range(len(trainData) - 1):
         t1 = trainData[i].split('/')[1].strip()
@@ -45,60 +43,45 @@ def trainTransition(trainData):
         else:
             ttCounts[t1] = {t2:1}
 
+
     for t1 in ttCounts:
         totalLog = log(sum(ttCounts[t1].values()))
         tt[t1] = {}
         for t2 in ttCounts[t1]:
-            tt[t1][t2] = log(ttCounts[t1][t2]) - totalLog if ttCounts[t1][t2] > 0 else None
+            tt[t1][t2] = log(ttCounts[t1][t2]) - totalLog if ttCounts[t1][t2] > 0 else -float('inf')
 
 
 def getViterbiTags(words):
-    bt = [dict.fromkeys(tagSet, '###')]
+    tmp = [dict.fromkeys(tagSet, '###')]
     trellis = [{}]
+
+    bt = dict.fromkeys(tagSet, ['###'])
 
     #populate first column in trellis
     for tag in tagSet:
         emission = tw[tag][words[1].split('/')[0].strip()]
         transition = tt['###'][tag]
-        trellis[0][tag] = emission + transition if emission != None and transition!= None else None
+        trellis[0][tag] = emission + transition
+        bt[tag] = bt['###'] + [tag]
 
-    for word in words[2:]:
-        bt.append({})
+    for line in words[2:]:
+        word = line.split('/')[0].strip()
         col = {}
+        newbt = {}
         prevCol = trellis[-1]
         for tag in tagSet:
-            maxTag = prevCol['###']
-            maxVal = -float("inf")
-            for tPrev in prevCol:
-                if prevCol[tPrev] != None and tt[tPrev][tag] != None and prevCol[tPrev] + tt[tPrev][tag] > maxVal:
-                    maxTag = tPrev
-                    maxVal = prevCol[tPrev] + tt[tPrev][tag]
-            bt[-1][tag] = maxTag
-
-
-            emission = tw[tag][word.split('/')[0].strip()]
-            col[tag] = emission + maxVal if emission != None and maxVal != None else None
+            (prob, prevTag) = max([(prevCol[prevTag] + tt[prevTag][tag] + tw[tag][word] , prevTag) for prevTag in tagSet])
+            col[tag] = prob
+            newbt[tag] = bt[prevTag] + [tag]
+        bt = newbt
         trellis.append(col)
 
-    print(len(bt))
-    print(len(words))
+    correct = 0
+    for i in range(len(words)):
+        if bt['###'][i] == words[i].split('/')[1].strip() and bt['###'][i] != '###':
+            correct += 1
 
-    tagSeq = ['###']
-    correctCount = 0
-    for i in range(len(words) - 1, 0, -1):
-        col = bt[i - 1]
-        tag = col[tagSeq[-1]]
-        if tag == words[i].split('/')[1].strip():
-            correctCount += 1
-        tagSeq.append(tag)
-
-    return (tagSeq, correctCount/len(words))
-
-
-
-
-
-
+    return (bt['###'], 100 * correct/(len(words) - 2))
 
 
 def main():
@@ -109,15 +92,14 @@ def main():
         trainEmission(trainData)
         trainTransition(trainData)
         trainFile.close()
-        #print(tt)
-        #print(tw)
 
         testFile = open('data/ic/{}.txt'.format(argv[2]), 'r')
         testData = [line for line in testFile]
 
-        print('\nViterbi Tags')
-        for tag in getViterbiTags(testData):
-            print(tag)
+        (tags, accuracy) = getViterbiTags(testData)
+        print('Tagging accuracy (Viterbi decoding): {}%'.format(accuracy))
+
+
         testFile.close()
 
     else:
